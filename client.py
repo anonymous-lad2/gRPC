9f4a67f483e1f076bc89550effd6b34d07b2b143
satyam <anonymouslad540@gmail.com>
2026-09-13 14:29:44 +0530

    server streaming

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 import grpc
 
-from generated.user_pb2 import UserRequest
+from generated.user_pb2 import UserRequest, ListUsersRequest
 from generated.user_pb2_grpc import UserServiceStub
 
 def run():
@@ -28,8 +28,16 @@ def run():
             print("SUCCESS:", response)
         except grpc.RpcError as e:
             print("ERROR:", e.code(), e.details())
-
-
         
+        # Test 4: list users
+        try:
+            print("--- ListUsers streaming ---")
+            response = stub.ListUsers(ListUsersRequest(page_size=3), metadata=metadata, timeout=10)
+            for user in response:
+                print("received user:", user)
+            print("--- ListUsers streaming done ---")
+        except grpc.RpcError as e:
+            print("ERROR:", e.code(), e.details())
+
 if __name__ == "__main__":
     run()
--- generated/user_pb2.py
+++ generated/user_pb2.py
@@ -24,7 +24,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\nuser.proto\x12\x04user\"/\n\x04User\x12\n\n\x02id\x18\x01 \x01(\x05\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\r\n\x05\x65mail\x18\x03 \x01(\t\"\x19\n\x0bUserRequest\x12\n\n\x02id\x18\x01 \x01(\x05\x32\x37\n\x0bUserService\x12(\n\x07GetUser\x12\x11.user.UserRequest\x1a\n.user.Userb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\nuser.proto\x12\x04user\"/\n\x04User\x12\n\n\x02id\x18\x01 \x01(\x05\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\r\n\x05\x65mail\x18\x03 \x01(\t\"\x19\n\x0bUserRequest\x12\n\n\x02id\x18\x01 \x01(\x05\"%\n\x10ListUsersRequest\x12\x11\n\tpage_size\x18\x01 \x01(\x05\x32j\n\x0bUserService\x12(\n\x07GetUser\x12\x11.user.UserRequest\x1a\n.user.User\x12\x31\n\tListUsers\x12\x16.user.ListUsersRequest\x1a\n.user.User0\x01\x62\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -35,6 +35,8 @@ if not _descriptor._USE_C_DESCRIPTORS:
   _globals['_USER']._serialized_end=67
   _globals['_USERREQUEST']._serialized_start=69
   _globals['_USERREQUEST']._serialized_end=94
-  _globals['_USERSERVICE']._serialized_start=96
-  _globals['_USERSERVICE']._serialized_end=151
+  _globals['_LISTUSERSREQUEST']._serialized_start=96
+  _globals['_LISTUSERSREQUEST']._serialized_end=133
+  _globals['_USERSERVICE']._serialized_start=135
+  _globals['_USERSERVICE']._serialized_end=241
 # @@protoc_insertion_point(module_scope)
